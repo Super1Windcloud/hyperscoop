d8b12411bb530e29e9a2328a004dfdaa16c00273
SuperWindCloud <13111080+yu-zhengui@user.noreply.gitee.com>
2024-11-09 11:37:12 +0800

    :panda_face:     update script for URL and version

--- script/hash.py
+++ script/hash.py
@@ -2,6 +2,40 @@
 import  os
 import  json
 
+#  更新文件的hash version 和 下载URL
+
+def  update_version_and_url() :
+      version_toml= r'A:\Rust_Project\hyperscoop\Cargo.toml'
+      with open(version_toml, "r" ,encoding="utf-8") as f:
+          data =  f.readlines()
+          for line in data:
+              if  line.startswith("version") :
+                  version =  line.split("=")[1].strip()
+                  break
+      manifest_path1 = os.path.join(os.path.dirname(os.path.dirname(__file__)),
+      r"hyperscoop_source_bucket/bucket/hyperscoop.json")
+      manifest_path2 = r'A:\Scoop\buckets\hyperscoop\bucket\hyperscoop.json'
+      with open(manifest_path2, "r" ,encoding="utf-8") as f:
+          data =  json.load(f)
+          old_version = data["version"]
+          data["version"] = version.replace('"', '')
+          data["url"]  =data["url"].replace(old_version, version).replace('"', '')
+          with open(manifest_path2, "w", encoding="utf-8") as writer :
+                 json.dump(data, writer , ensure_ascii=False , indent=4)  # 禁用 ASCII 编码以保留非 ASCII 字符（如中文） )
+
+
+
+      with open(manifest_path1, "r" ,encoding="utf-8") as f:
+              data =  json.load(f)
+              old_version = data["version"]
+              data["version"] = version.replace('"', '')
+              data["url"]  =data["url"].replace(old_version, version).replace('"', '')
+              with open(manifest_path1, "w", encoding="utf-8") as writer :
+                     json.dump(data, writer , ensure_ascii=False , indent=4)  # 禁用 ASCII 编码以保留非 ASCII 字符（如中文） )
+
+
+
+
 
 def  calculate_hash(file_path):
     """Calculate the hash value of a file"""
@@ -39,9 +73,9 @@ def  write_scoop_bucket(   hash_value   ) :
 if __name__ == '__main__':
     debug_file_path = r"A:\Rust_Project\hyperscoop\target\debug\hyperscoop.exe"
     release_file_path = r"A:\Rust_Project\hyperscoop\target\release\hyperscoop.exe"
-    #result =calculate_hash(release_file_path)
-    result = None
-    result =  calculate_hash(debug_file_path)  if result is None else result
+    result =calculate_hash(release_file_path)
+    # result =  calculate_hash(debug_file_path)
     write_to_manifest(result)  # 将哈希值写入 manifest 文件
     write_scoop_bucket ( result )  # 将哈希值写入 scoop bucket
+    update_version_and_url()  # 更新版本号和下载URL
 
