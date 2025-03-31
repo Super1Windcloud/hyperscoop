8c35ef59851be16373ca9f6d35717fa562ffbec4
SuperWindCloud <13111080+yu-zhengui@user.noreply.gitee.com>
2025-03-31 23:02:34 +0800

    :panda_face:     update hash

--- script/hash.py
+++ script/hash.py
@@ -1,7 +1,7 @@
 ﻿import   hashlib
 import  os
-import  json
-
+import http.client
+import json
 def   get_gitee_latest_version () :
       host = "gitee.com"
       url = "/api/v5/repos/SuperWindcloud/hyperscoop/releases/latest"
@@ -16,7 +16,7 @@ def   get_gitee_latest_version () :
       json_data = json.dumps(data)
 
       conn = http.client.HTTPSConnection(host)
-      conn.request("POST", url, body=json_data, headers=headers)
+      conn.request("GET", url, body=json_data, headers=headers)
 
       response = conn.getresponse()
       print("Status:", response.status, response.reason)
@@ -25,14 +25,18 @@ def   get_gitee_latest_version () :
       return response.get("tag_name")
 
 
-def  update_version_and_url() :
+
+def get_version_from_cargo():
       version_toml= r'A:\Rust_Project\hyperscoop\Cargo.toml'
       with open(version_toml, "r" ,encoding="utf-8") as f:
-          data =  f.readlines()
-          for line in data:
-              if  line.startswith("version") :
-                  version =  line.split("=")[1].strip()
-                  break
+            data =  f.readlines()
+            for line in data:
+                if  line.startswith("version") :
+                    version =  line.split("=")[1].strip()
+                    break
+      return version
+def  update_version_and_url() :
+      version= get_gitee_latest_version();
       manifest_path1 = os.path.join(os.path.dirname(os.path.dirname(__file__)),  r"hyperscoop_source_bucket/bucket/hp.json")
       manifest_path2 = r'A:\Scoop\buckets\hp\bucket\hp.json'
       with open(manifest_path1, "r" ,encoding="utf-8") as f:
@@ -91,11 +95,19 @@ def  write_scoop_bucket(   hash_value   ) :
                  json.dump(data, writer,  ensure_ascii=False, indent=4)
 
 
+def main():
+      release_file_path = r"A:\Rust_Project\hyperscoop\target\release\hp.exe"
+      result =calculate_hash(release_file_path)
+      write_to_manifest(result)  # 将哈希值写入 manifest 文件
+      write_scoop_bucket ( result )  # 将哈希值写入 scoop bucket
+      update_version_and_url()  # 更新版本号和下载URL
+
+
+def test():
+      version = get_version_from_cargo()
+      print(version)
+      version = get_gitee_latest_version()
+      print(version)
 
 if __name__ == '__main__':
-    release_file_path = r"A:\Rust_Project\hyperscoop\target\release\hp.exe"
-    result =calculate_hash(release_file_path)
-    write_to_manifest(result)  # 将哈希值写入 manifest 文件
-    write_scoop_bucket ( result )  # 将哈希值写入 scoop bucket
-    update_version_and_url()  # 更新版本号和下载URL
-
+    main()
