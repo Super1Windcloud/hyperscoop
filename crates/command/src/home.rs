dba9adce945a931ee2cdce33a59606b3b547816d
SuperWindCloud <13111080+yu-zhengui@user.noreply.gitee.com>
2025-04-10 22:02:47 +0800

    :panda_face:       add   bin trait

--- crates/command/src/home.rs
+++ crates/command/src/home.rs
@@ -1,6 +1,7 @@
 use webbrowser;
+use crate::utils::utility::is_valid_url;
 
-pub fn open_home_page(bucket_paths: Vec<String>, name: String) {
+pub fn _open_home_page(bucket_paths: Vec<String>, name: String) {
     for bucket_path in bucket_paths.iter() {
         let manifest_path = bucket_path.clone() + "\\bucket";
         log::info!("manifest_path: {}", manifest_path);
@@ -15,65 +16,47 @@ pub fn open_home_page(bucket_paths: Vec<String>, name: String) {
                 }
                 let content = std::fs::read_to_string(file).unwrap();
                 let serde_obj = serde_json::from_str::<serde_json::Value>(&content).unwrap();
-                let url = serde_obj["homepage"].as_str().unwrap();
+                let url = serde_obj["homepage"].as_str().unwrap_or_default();
                 #[cfg(debug_assertions)]
                 dbg!(url);
                 webbrowser::open(url).expect("Failed to open page");
-
                 return;
             }
         }
     }
 }
 
+pub fn open_home_page(bucket_paths: Vec<String>, name: String)->anyhow::Result<()> {
+    let  found =  bucket_paths.iter().find_map(|bucket_path| {
+        let manifest_path = format!("{}\\bucket", bucket_path);
+        log::info!("manifest_path: {}", manifest_path);
 
-// pub fn _open_home_page(bucket_paths: Vec<String>, name: String) {
-//   use rayon::prelude::*;
-// 
-//   bucket_paths.par_iter().find_map_any(|bucket_path| {
-//     let manifest_path = bucket_path.clone() + "\\bucket";
-//     log::info!("manifest_path: {}", manifest_path);
-// 
-//     let dir_entries = match std::fs::read_dir(&manifest_path) {
-//       Ok(entries) => entries,
-//       Err(_) => return None,
-//     };
-// 
-//     dir_entries.filter_map(|entry| { 
-//       let  entry = entry.unwrap();
-//       let file_type =&entry.file_type().unwrap();
-//       let file  = entry.path();
-//       let file_str = file.as_path().display().to_string();
-//       if  file_type.is_file() && file_str.ends_with(".json") {
-//         let file_name = match file.file_stem().and_then(|s| s.to_str()) {
-//           Some(name) => name.split('/').last().unwrap(),
-//           None => return None,
-//         };
-// 
-//         if file_name != name {
-//           return None;
-//         }
-// 
-//         match std::fs::read_to_string(&file) {
-//           Ok(content) => {
-//             match serde_json::from_str::<serde_json::Value>(&content) {
-//               Ok(serde_obj) => serde_obj["homepage"].as_str().map(|s| s.to_string()),
-//               Err(_) => None,
-//             }
-//           }
-//           Err(_) => None,
-//         }
-//       } else {
-//         None
-//       }
-//     }).find_any (|url| {
-//       if let Some(url) = url {
-//         #[cfg(debug_assertions)]
-//         dbg!(url);
-//         webbrowser::open(url).is_ok()
-//       } else {
-//         false
-//       }
-//     })
-//   });
-// }
+        std::fs::read_dir(manifest_path)
+            .ok()?
+            .filter_map(|entry| {
+                let file = entry.ok()?.path();
+                if !file.is_file() || !file.to_string_lossy().ends_with(".json") {
+                    return None;
+                }
+
+                let file_name = file.file_stem()?.to_str()?;
+                if file_name != name {
+                    return None;
+                }
+
+                let content = std::fs::read_to_string(&file).ok()?;
+                let serde_obj = serde_json::from_str::<serde_json::Value>(&content).ok()?;
+                let url = serde_obj["homepage"].as_str().unwrap_or_default();
+ 
+                if  !url.is_empty() && is_valid_url(&url) {
+                  webbrowser::open(url).ok()?;
+                }
+                Some(())
+            })
+            .next()
+    });
+   if found.is_some() {
+    std::process::exit(0);
+   } 
+   Ok(())
+}
--- src/hyperscoop_middle/invoke_home.rs
+++ src/hyperscoop_middle/invoke_home.rs
@@ -3,9 +3,8 @@ use command_util_lib::buckets::get_buckets_path;
 use command_util_lib::home::open_home_page;
 pub fn execute_home_command(home: HomeArgs) -> Result<(), anyhow::Error> {
     if let Some(name) = home.name {
-        log::info!("execute_home_command called with name: {}", name);
-        let bucket_path = get_buckets_path().expect("Failed to get buckets path");
-        open_home_page(bucket_path, name);
+        let bucket_path = get_buckets_path()?;
+        open_home_page(bucket_path, name)?;
     }
     Ok(())
 }
