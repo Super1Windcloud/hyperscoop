a9b10a074a2b0cd11dd6b22f4b363607a4c984e7
SuperWindCloud <13111080+yu-zhengui@user.noreply.gitee.com>
2025-04-11 21:29:45 +0800

    :panda_face:     update hash

--- script/hash.py
+++ script/hash.py
@@ -70,7 +70,7 @@ def  calculate_hash(file_path):
     with open(file_path, 'rb') as f:
         #  scoop 默认使用sha256 哈希算法
         return hashlib.sha256(f.read()).hexdigest()
-def  write_to_manifest(hash_value):
+def  write_to_manifest(x64 ,arm64,x86  ):
     """Write the hash value to the manifest file"""
     manifest_path = os.path.join(os.path.dirname(os.path.dirname(__file__)),
     r"hyperscoop_source_bucket/bucket/hp.json")
@@ -78,28 +78,44 @@ def  write_to_manifest(hash_value):
         return None
     with open(manifest_path, "r" ,encoding="utf-8") as f:
         data =  json.load(f)
-        data["hash"] = hash_value
+        data["hash"] = x64
+        data["architecture"]["64bit"]["hash"] = x64
+        data["architecture"]["arm64"]["hash"] = arm64
+        data["architecture"]["32bit"]["hash"] = x86
+        data["architecture"]["64bit"]["url"] = data["url"]
+        data["architecture"]["arm64"]["url"] = data["url"]
+        data["architecture"]["32bit"]["url"] = data["url"]
         with open(manifest_path, "w", encoding="utf-8") as writer :
                  json.dump(data, writer,  ensure_ascii=False, indent=4)
 
 
-def  write_scoop_bucket(   hash_value   ) :
+def  write_scoop_bucket(   x64 ,arm64,x86    ) :
      """Write the hash value to the scoop bucket"""
      hyperscoop_bucekt = r'A:\Scoop\buckets\hp\bucket\hp.json'
      if not os.path.isfile(hyperscoop_bucekt):
          return None
      with open(hyperscoop_bucekt, "r" ,encoding="utf-8") as f:
          data =  json.load(f) ; print(data["hash"])
-         data["hash"] = hash_value
+         data["hash"] = x64
+         data["architecture"]["64bit"]["hash"] = x64
+         data["architecture"]["arm64"]["hash"] = arm64
+         data["architecture"]["32bit"]["hash"] = x86
+         data["architecture"]["64bit"]["url"] = data["url"]
+         data["architecture"]["arm64"]["url"] = data["url"]
+         data["architecture"]["32bit"]["url"] = data["url"]
          with open(hyperscoop_bucekt, "w", encoding="utf-8") as writer :
                  json.dump(data, writer,  ensure_ascii=False, indent=4)
 
 
 def main():
-      release_file_path = r"A:\Rust_Project\hyperscoop\target\release\hp.exe"
-      result =calculate_hash(release_file_path)
-      write_to_manifest(result)  # 将哈希值写入 manifest 文件
-      write_scoop_bucket ( result )  # 将哈希值写入 scoop bucket
+      release_x64 = r"A:\Rust_Project\hyperscoop\target\x86_64-pc-windows-msvc\release\hp.exe"
+      release_arm64 = r"A:\Rust_Project\hyperscoop\target\i686-pc-windows-msvc\release\hp.exe"
+      release_x86  = r"A:\Rust_Project\hyperscoop\target\aarch64-pc-windows-msvc\release\hp.exe"
+      result1 =calculate_hash(release_x64)
+      result2 =calculate_hash(release_arm64)
+      result3 =calculate_hash(release_x86)
+      write_to_manifest(result1, result2 ,result3)  # 将哈希值写入 manifest 文件
+      write_scoop_bucket ( result1,result2 , result3 )  # 将哈希值写入 scoop bucket
       update_version_and_url()  # 更新版本号和下载URL
 
 
