6ecf8c0d21b3295e474346b9139906b0c5eefcd7
SuperWindCloud <13111080+yu-zhengui@user.noreply.gitee.com>
2025-04-12 12:42:54 +0800

    :panda_face:   add token\

--- build.rs
+++ build.rs
@@ -1,11 +1,11 @@
 ﻿use std::env;
 
-
 fn get_gitee_env_path() ->String {
   let cwd = env::current_dir().unwrap();
   let env= cwd.join(".env");
   if !env.exists() {
-    panic!("{}", "当前目录下不存在.env".to_string());
+    eprintln!("{}", "当前目录下不存在.env".to_string()); 
+    return "".to_string(); 
   }
   let  env =std::fs::read_to_string(&env).unwrap();
   env.trim().to_string()
@@ -15,7 +15,8 @@ fn  get_github_token_path()->String {
   let cwd = env::current_dir().unwrap();
   let env= cwd.join(".github_token");
   if !env.exists() {
-    panic!("{}", "当前目录下不存在.github_token".to_string());
+    eprintln!("{}", "当前目录下不存在.github_token".to_string()); 
+    return "".to_string();
   }
   let  env =std::fs::read_to_string(&env).unwrap(); 
   env.trim().to_string()
@@ -29,10 +30,15 @@ fn main() {
         .unwrap_or_default();
     let lang_prefix = lang.split("_").next().unwrap_or("en");
     let gitee_env= get_gitee_env_path();
-   let  github_token = get_github_token_path(); 
+    let  github_token = get_github_token_path();  
     println!("cargo:rustc-env=BUILD_SYSTEM_LANG={}", lang_prefix);
     println!("cargo:rustc-env=GITEE_TOKEN={}", gitee_env);
-    println!("cargo:rustc-env=GITHUB_TOKEN={}", github_token);
+    println!("cargo:rustc-env=GITHUB_TOKEN={}", github_token);  
+    if  ! github_token.is_empty()  &&  !gitee_env.is_empty() {
+        println!("cargo:rustc-cfg=token_local");
+    } else {  
+      println!("cargo:rustc-cfg=token_cloud");
+    }
     if lang_prefix == "zh" {
         println!("cargo:rustc-cfg=system_lang_zh");
     }
--- src/check_self_update.rs
+++ src/check_self_update.rs
@@ -1,10 +1,12 @@
+use std::cmp::max;
+use std::env;
 use crate::Cli;
 use anyhow::{anyhow, bail};
 use clap::CommandFactory;
 use crossterm::style::Stylize;
 use reqwest::Client;
+use reqwest::header::USER_AGENT;
 use serde::{Deserialize, Serialize};
-
 #[allow(clippy::unsafe_derive_deserialize)]
 #[allow(dead_code)]
 #[derive(Debug, Clone, Default, Deserialize)]
@@ -14,15 +16,17 @@ struct GiteeRelease {
     body: Option<String>,
 }
 
+
 pub async fn auto_check_hp_update() -> anyhow::Result<bool> {
     let cmd = Cli::command();
     let version = cmd.get_version().ok_or(anyhow!("hp version is empty"))?;
 
     let latest_version = get_latest_version_from_gitee().await?;
-    let latest_github_version = get_latest_version_from_github().await?;
-    println!("Latest version: {}", latest_github_version);
-    if version.to_string() < latest_version {
-        println!("{}", format!("发现hp新版本 {latest_version},请访问https://gitee.com/SuperWindcloud/hyperscoop/releases").yellow().bold());
+    let latest_github_version = get_latest_version_from_github().await?; 
+    let  max_version  =  max(latest_version, latest_github_version);
+    
+    if version.to_string() < max_version {
+        println!("{}", format!("发现hp新版本 {max_version},请访问https://gitee.com/SuperWindcloud/hyperscoop/releases").yellow().bold());
         Ok(true)
     } else {
         Ok(false)
@@ -33,10 +37,10 @@ pub async fn auto_check_hp_update() -> anyhow::Result<bool> {
 struct GithubRelease {
     tag_name: String,
 }
-async fn get_latest_version_from_github() -> anyhow::Result<String> {
-    let token = std::env::var("GITHUB_TOKEN").unwrap_or_default();
-    let local_token = std::fs::read_to_string(".github_token").unwrap_or_default();
-    if token.is_empty() && local_token.is_empty() {
+#[cfg(not(feature = "token_cloud"))]
+async fn get_latest_version_from_github() -> anyhow::Result<String> { 
+    let token =  include_str!("../.github_token").trim() ; 
+    if token.is_empty()   {
         bail!("GITHUB_TOKEN environment variable is empty");
     }
     let owner = "super1windcloud";
@@ -45,21 +49,59 @@ async fn get_latest_version_from_github() -> anyhow::Result<String> {
         "https://api.github.com/repos/{}/{}/releases/latest",
         owner, repo
     );
-
-    let response = reqwest::get(&url).await?;
-
-    let tags: GithubRelease = response.json().await.unwrap();
-
+   let client = Client::builder().user_agent(USER_AGENT).build()?;
+    let response =  client.get(&url).send().await?;
+    let tags: GithubRelease = response.json().await?;
     Ok(tags.tag_name)
 }
 
+#[cfg(feature = "token_cloud")]
+async fn get_latest_version_from_github() -> anyhow::Result<String> {
+  let token = std::env::var("MY_ACCESS_TOKEN").unwrap_or_default().trim().to_string();
+  if token.is_empty()   {
+    bail!("GITHUB_TOKEN environment variable is empty");
+  }
+  let owner = "super1windcloud";
+  let repo = "hp";
+  let url = format!(
+    "https://api.github.com/repos/{}/{}/releases/latest",
+    owner, repo
+  );
+  let client = Client::builder().user_agent(USER_AGENT).build()?;
+  let response =  client.get(&url).send().await?;
+  let tags: GithubRelease = response.json().await?;
+  Ok(tags.tag_name)
+}
+
+#[cfg(not(feature = "token_cloud"))]
+async fn get_latest_version_from_gitee() -> anyhow::Result<String> {
+  let access_token =  include_str!("../.env").trim() ;
+    if access_token.is_empty() {
+        bail!("GITEE_TOKEN environment variable is empty");
+    }
+    let client = Client::new();
+    let response = client
+        .get("https://gitee.com/api/v5/repos/superwindcloud/hyperscoop/releases/latest")
+        .header("Content-Type", "application/json;charset=UTF-8")
+        .query(&[("access_token", access_token)])
+        .send()
+        .await?;
+    if !response.status().is_success() {
+        return Err(anyhow::anyhow!("请求失败: {}", response.status()));
+    }
+    let release = response.json::<GiteeRelease>().await?;
+    let gitee_tag = release.tag_name;
+
+    Ok(gitee_tag)
+}
+
+#[cfg(feature = "token_cloud")]
 async fn get_latest_version_from_gitee() -> anyhow::Result<String> {
     let access_token = std::env::var("GITEE_TOKEN");
     if access_token.is_err() {
         bail!("GITEE_TOKEN environment variable is empty");
-    } 
-    let access_token = access_token.unwrap().trim().to_string(); 
-     println!("Getting gitee token {access_token}");
+    }
+    let access_token = access_token?.trim().to_string();
     let client = Client::new();
     let response = client
         .get("https://gitee.com/api/v5/repos/superwindcloud/hyperscoop/releases/latest")
@@ -85,8 +127,9 @@ mod test_auto_update {
     }
     #[tokio::test]
     async  fn test_github_api() {
-        use super::*; 
+        use super::*;
         let  result  = get_latest_version_from_gitee().await.unwrap();
-        println!("{}", result); 
+        let  github_version = get_latest_version_from_github().await.unwrap();
+        println!("Latest  github version: {}", github_version);
     }
 }
